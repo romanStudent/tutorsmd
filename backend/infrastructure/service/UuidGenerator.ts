6f6c40b719e7708df567bae44346fd3b09208575
Roman <ra.ivanov1405@gmail.com>
2026-04-26 10:11:29 +0200

    UUID Generate to decomposition

--- backend/infrastructure/service/UuidGenerator.ts
+++ backend/infrastructure/service/UuidGenerator.ts
@@ -1,9 +1,9 @@
 // infrastructure/service/UuidGenerator.ts
 
 import { v4 as uuidv4 } from 'uuid';
-import { IUUIdGenerator } from '../../application/ports/IUUIDGenerator';
+import { IUUIDGenerator } from '../../application/ports/IUUIDGenerator';
 
-export class UuidGenerator implements IUUIdGenerator {
+export class UuidGenerator implements IUUIDGenerator {
   generate(): string {
     return uuidv4();
   }
