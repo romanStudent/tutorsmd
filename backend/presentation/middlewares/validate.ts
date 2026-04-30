c14ade0e708161a684bdd44ded4790516f1984db
Roman <ra.ivanov1405@gmail.com>
2026-04-30 16:35:46 +0200

    Bibliothek ZOD for Validation Data in Controller

---
+++ backend/presentation/middlewares/validate.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Response, Request } from 'express';
+import { ZodSchema } from 'zod';
+
+export const validate = (schema: ZodSchema) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body);
+    if (!result.success) {
+      return res.status(400).json({
+        message: 'Validation error',
+        errors: result.error.flatten().fieldErrors,
+      });
+    }
+    req.body = result.data;
+    next();
+};
