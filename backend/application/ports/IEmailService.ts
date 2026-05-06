a2a587bc4a0e88b104c212b6ee91f231564ea815
Roman <ra.ivanov1405@gmail.com>
2026-05-06 17:58:54 +0200

    feat: language in parameter and now can emails in different languages to send

--- backend/application/ports/IEmailService.ts
+++ backend/application/ports/IEmailService.ts
@@ -1,6 +1,6 @@
 export interface IEmailService {
   sendActivationLink(email: string, link: string, language?: string): Promise<void>;
   sendPasswordResetLink(email: string, link: string, language?: string): Promise<void>;
-  sendEmailChangeConfirmation(email: string, link: string): Promise<void>;
-  sendLessonReminder(email: string, link: string, time: string): Promise<void>;
+  sendEmailChangeConfirmation(email: string, link: string, language?: string): Promise<void>;
+  sendLessonReminder(email: string, link: string, time: string, language?: string): Promise<void>;
 }
