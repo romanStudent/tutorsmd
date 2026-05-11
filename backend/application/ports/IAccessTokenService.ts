0ecd108bec41164c650388beabd00026aba8804a
Roman <ra.ivanov1405@gmail.com>
2026-05-11 15:00:30 +0200

    chore: move Access token interface in folder token

--- backend/application/ports/IAccessTokenService.ts
+++
@@ -1,6 +0,0 @@
-import { AccessTokenPayload } from '../../domain/value-objects/AccessToken';
-
-export interface IAccessTokenService {
-  generateAccessToken(payload: AccessTokenPayload): string;
-  verifyAccessToken(token: string): AccessTokenPayload | null;
-}
