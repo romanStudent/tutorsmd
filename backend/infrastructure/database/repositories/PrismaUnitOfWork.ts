cd13bec9cb99cdc133ed087a7f7cad2cb8089f86
Roman <ra.ivanov1405@gmail.com>
2026-05-16 23:37:35 +0200

    noch Änderungen für transaction

--- backend/infrastructure/database/repositories/PrismaUnitOfWork.ts
+++ backend/infrastructure/database/repositories/PrismaUnitOfWork.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from '../../../generated/prisma';
-import { IUnitOfWork } from '../../../application/ports/IUnitOfWork';
+import { ITransactionClient, IUnitOfWork } from '../../../application/ports/IUnitOfWork';
 
 export class PrismaUnitOfWork implements IUnitOfWork {
   constructor(private readonly prisma: PrismaClient) {}
 
-  async run<T>(work: () => Promise<T>): Promise<T> {
-    return this.prisma.$transaction(async () => {
-      return work();
+  async run<T>(work: (tx: ITransactionClient) => Promise<T>): Promise<T> {
+    return this.prisma.$transaction(async (tx: ITransactionClient) => {
+      return work(tx);
     });
   }
 }
