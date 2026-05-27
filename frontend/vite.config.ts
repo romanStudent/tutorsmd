7a8087ff06806369f1579702790a8a908063ebfc
Roman <ra.ivanov1405@gmail.com>
2026-05-27 18:06:13 +0200

    fix: deploy configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    visualizer({
+filename: 'dist/stats.html',
+open: true,
+gzipSize: true,
+}),],
 
   resolve: {
     alias: {
